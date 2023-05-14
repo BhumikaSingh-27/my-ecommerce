45322799f99c32c813b3e07674ce930211330aa0
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-05-14 13:03:14 +0530

    style: cart product card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
+import CartPrice from "./pages/Cart/components/CartPrice";
+import CartProduct from "./pages/Cart/components/CartProduct";
 import Home from "./pages/Home/Home";
 import Filter from "./pages/Product/components/Filter";
 import ProductCard from "./pages/Product/components/ProductCard";
@@ -13,7 +15,9 @@ function App() {
       <Home />
       {/* <Login /> */}
       {/* <div><Signup /></div> */}
-      <ProductCard />
+      {/* <ProductCard /> */}
+      <CartProduct />
+      {/* <CartPrice /> */}
     </div>
   );
 }
---
+++ src/pages/Cart/components/CartPrice.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const CartPrice = () => {
+  return (
+    <div>
+      <div className="cart-price-details">
+        <h3>Price Details</h3>
+        <hr />
+        <div className="cart-price-flex">
+          <div>Price</div>
+          <div>&#x20B9;200</div>
+        </div>
+        <div className="cart-price-flex">
+          <div>Discount</div>
+          <div>&#x20B9;200</div>
+        </div>
+        <div className="cart-price-flex">
+          <div>Delivery Charges</div>
+          <div>&#x20B9;200</div>
+        </div>
+        <hr />
+        <div className="cart-price-flex">
+          <h3>TOTAL AMOUNT</h3>
+          <h3>&#x20B9;200</h3>
+        </div>
+        <hr />
+        <span className="cart-price-message">You will save rs 200 on this order</span>
+        <button className="add-btn">Place order</button>
+      </div>
+    </div>
+  );
+};
+
+export default CartPrice;
