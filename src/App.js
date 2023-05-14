b34be8a23f9c9928a61a91080b7ba19cacec7746
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-05-14 09:54:04 +0530

    style: product card

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
 import Home from "./pages/Home/Home";
+import Filter from "./pages/Product/components/Filter";
+import ProductCard from "./pages/Product/components/ProductCard";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
-      <Login />
-  {/* <div><Signup /></div> */}
-      
+      {/* <Login /> */}
+      {/* <div><Signup /></div> */}
+      <ProductCard />
     </div>
   );
 }
